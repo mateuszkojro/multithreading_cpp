6ff7d3ba052231cd859107770948bd11e1ba4a01
mateuszkojro <mateusz.kojro@unilodz.eu>
2019-11-10 12:17:06 +0100

    multithreading c++

--- main.cpp
+++ main.cpp
@@ -1,24 +1,48 @@
 #include <thread>
 #include <iostream>
 
+#define t
+
 void  foo(int x){
-    for (int i = 0; i < 10;++i){
-        std::cout << x <<"  " << i << std::endl;
+    long long unsigned int wynik =0;
+    for (int i = 0; i < 2000000000;++i){
+        wynik += i;
     }
     
+    std::cout << x << " " << wynik << std::endl;
 
 }
 
-
+#ifdef t
 
 
 int main(){
 
-    std::thread thread1(foo,1);
-    std::thread thread2(foo,2);    
-
-    thread1.join();
-    thread2.join();
+    foo(1);
+    foo(2);
+    foo(3);
+    foo(4);
 
     return 0;
 }
+
+#endif
+
+#ifndef t
+
+int main(){
+
+    std::thread thread1(foo,1);
+    std::thread thread2(foo,2);
+    std::thread thread3(foo,2);    
+    std::thread thread4(foo,2);
+
+    thread1.join();
+    thread2.join();
+    thread3.join();
+    thread4.join();
+
+    return 0;
+}
+
+#endif
